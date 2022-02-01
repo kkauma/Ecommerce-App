c1cc490dbbd94a084a4d7e4c22a802f0686cc47e
kkauma <kylekauma@gmail.com>
2022-02-01 12:03:55 -0600

    add images and re-size to specs of finished app

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -34,6 +34,7 @@ class Directory extends React.Component {
           size: "large",
           id: 4,
           linkUrl: "shop/womens",
+          size: "large",
         },
         {
           title: "mens",
@@ -41,6 +42,7 @@ class Directory extends React.Component {
           size: "large",
           id: 5,
           linkUrl: "shop/mens",
+          size: "large",
         },
       ],
     };
@@ -49,8 +51,8 @@ class Directory extends React.Component {
   render() {
     return (
       <div className="directory-menu">
-        {this.state.sections.map(({ title, imageUrl, id }) => (
-          <MenuItem key={id} title={title} />
+        {this.state.sections.map(({ title, imageUrl, id, size }) => (
+          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
         ))}
       </div>
     );
