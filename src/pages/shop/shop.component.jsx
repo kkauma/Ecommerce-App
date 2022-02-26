b8cf92e9eafcff4b2611bb531faa9c4a3f19c888
kkauma <kylekauma@gmail.com>
2022-02-26 12:22:11 -0600

    build collection preview component and add to shop component

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -2,7 +2,8 @@ import React from "react";
 
 import SHOP_DATA from "./shop.data";
 
-// Class Component
+import CollectionPreview from "../../components/collection-preview/collection-preview.component";
+
 class ShopPage extends React.Component {
   constructor(props) {
     super(props);
@@ -12,7 +13,14 @@ class ShopPage extends React.Component {
     };
   }
   render() {
-    return <div>SHOP PAGE</div>;
+    const { collections } = this.state;
+    return (
+      <div className="shop-page">
+        {collections.map(({ id, ...otherCollectionProps }) => (
+          <CollectionPreview key={id} {...otherCollectionProps} />
+        ))}
+      </div>
+    );
   }
 }
 
